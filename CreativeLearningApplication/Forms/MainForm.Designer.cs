83e6bd20797b202d2076eb3cb0544bbb1fd62a4b
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-12 10:36:09 +0200

    Image is added

--- CreativeLearningApplication/Forms/MainForm.Designer.cs
+++ CreativeLearningApplication/Forms/MainForm.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tabControl = new System.Windows.Forms.TabControl();
             this.sourceTabPage = new System.Windows.Forms.TabPage();
             this.destinationTabPage = new System.Windows.Forms.TabPage();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
             this.tabControl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,6 +44,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl.Controls.Add(this.sourceTabPage);
             this.tabControl.Controls.Add(this.destinationTabPage);
+            this.tabControl.ImageList = this.imageList;
             this.tabControl.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.tabControl.Location = new System.Drawing.Point(13, 13);
             this.tabControl.Name = "tabControl";
@@ -50,23 +54,34 @@
             // 
             // sourceTabPage
             // 
-            this.sourceTabPage.Location = new System.Drawing.Point(4, 22);
+            this.sourceTabPage.ImageIndex = 1;
+            this.sourceTabPage.Location = new System.Drawing.Point(4, 23);
             this.sourceTabPage.Name = "sourceTabPage";
             this.sourceTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.sourceTabPage.Size = new System.Drawing.Size(251, 152);
+            this.sourceTabPage.Size = new System.Drawing.Size(251, 151);
             this.sourceTabPage.TabIndex = 0;
             this.sourceTabPage.Text = "Source Options";
             this.sourceTabPage.UseVisualStyleBackColor = true;
             // 
             // destinationTabPage
             // 
-            this.destinationTabPage.Location = new System.Drawing.Point(4, 22);
+            this.destinationTabPage.ImageIndex = 3;
+            this.destinationTabPage.Location = new System.Drawing.Point(4, 23);
             this.destinationTabPage.Name = "destinationTabPage";
             this.destinationTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.destinationTabPage.Size = new System.Drawing.Size(192, 74);
+            this.destinationTabPage.Size = new System.Drawing.Size(251, 151);
             this.destinationTabPage.TabIndex = 1;
             this.destinationTabPage.Text = "Destination Options";
             this.destinationTabPage.UseVisualStyleBackColor = true;
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "Back16.bmp");
+            this.imageList.Images.SetKeyName(1, "Down16.bmp");
+            this.imageList.Images.SetKeyName(2, "Forward16.bmp");
+            this.imageList.Images.SetKeyName(3, "Up16.bmp");
             // 
             // MainForm
             // 
@@ -86,5 +101,6 @@
         private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage sourceTabPage;
         private System.Windows.Forms.TabPage destinationTabPage;
+        private System.Windows.Forms.ImageList imageList;
     }
 }
